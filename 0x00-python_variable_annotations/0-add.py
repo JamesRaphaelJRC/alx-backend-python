3f99184340eeac90bd668ed23b1dbef7ef628045
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-07 20:44:48 +0100

    initial commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' Defines a function that takes floats as arguments and returns a float '''
+
+
+def add(a: float, b: float) -> float:
+    ''' Returns float (a + b) '''
+    return a + b
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains my backend Short specialization tasks on python

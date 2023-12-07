310d6e7022d5de4fe0b772754ecc0b57a3084ad5
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-07 23:15:25 +0100

    task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 ''' Defines a type-annoted function 'sum_list' '''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     ''' Returns a sum of the elements in the input_list in float '''
     return sum(elem for elem in input_list)

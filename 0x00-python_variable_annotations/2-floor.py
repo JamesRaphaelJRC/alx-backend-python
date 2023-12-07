163e10abf173a184601a77e49acd24784082e1b7
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-07 21:38:11 +0100

    task 1

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+""" Defines a type-annoted function 'floor' """
 import math
-''' Defines a type-annoted function 'floor' '''
 
 
-def floor(n: float) -> float:
-    ''' Returns the floor of the float 'n' '''
+def floor(n: float) -> int:
+    """ Returns the floor(int) of the floating point number 'n' """
     return math.floor(n)

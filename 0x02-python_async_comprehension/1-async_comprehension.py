dde73eaa7a7590c6bfda25e89e6c623179ccf8ec
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-12 21:37:27 +0100

    task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 ''' Defines a coroutine 'async_comprehension' '''
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[async_generator]:
     ''' Collects 10 numbers using an async comprehensing over 
         async_generator and return the 10 numbers
     '''

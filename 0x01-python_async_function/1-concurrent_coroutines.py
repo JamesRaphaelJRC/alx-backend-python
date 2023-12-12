afbf12b8f87ea3538b272c7dfd5a64df02a97ae9
jamesraphaeljrc <jamesraphaeljrc@gmail.com>
2023-12-12 08:38:42 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     ''' Spawns the 'wait_random' async function 'n' number of times with the
         the specified 'max_delay'.
 
-        Return: A list of delays in ascending order
+        Return: A list of delays in ascending order.
     '''
     spawn_list = [wait_random(max_delay) for i in range(n)]
     delay_list = await asyncio.gather(*spawn_list)
